ad0217ed7956b9f742e51325fc683aa3a5111062
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2025-08-19 17:02:25 +0700

    Configure swagger for auth

--- src/EAMS.API/Program.cs
+++ src/EAMS.API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +22,45 @@ builder.Services.AddScoped<ICorrelationIdGenerator, CorrelationIdGenerator>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "EAMS API", Version = "v1" });
+    // OAuth2 authorization
+    var tenantId = builder.Configuration["AzureAd:TenantId"];
+    var authUrl = new Uri($"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/authorize");
+    var tokenUrl = new Uri($"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token");
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = authUrl,
+                TokenUrl = tokenUrl,
+                Scopes = new Dictionary<string, string>
+                {
+                    {builder.Configuration["SwaggerOAuth:Scopes"]!, "Access as user"}
+                }
+            }
+        }
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                }
+            },
+            new[] { builder.Configuration["SwaggerOAuth:Scopes"]! }
+        }
+    });
+});
 builder.Services.AddDbContext<EamsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -43,7 +82,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EAMS API");
+        c.OAuthClientId(builder.Configuration["SwaggerOAuth:ClientId"]!);
+        c.OAuthUsePkce();
+        c.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();

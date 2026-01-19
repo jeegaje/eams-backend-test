e174bf7b4b49ef055706b6f79293d02087c72c36
Anggajiyan Imanuddin <anggajiyan.imanuddin@opencloud.id>
2026-01-19 13:19:59 +0700

    testing log last

--- src/EAMS.API/Controllers/EnvController.cs
+++ src/EAMS.API/Controllers/EnvController.cs
@@ -82,14 +82,15 @@ namespace AMS.API.Controllers
             int id = 13;
             int userId = 11;
             int? organisationId = null;
-            var analyticsObj = new {
-                action = "testingAccommodation",
-                accommodationId = id,
-                userId = userId,
-                organisationId = organisationId
+
+            var logsObj = new LogsDto
+            {
+                Action = "testing2Accommodation",
+                AccommodationId = id,
+                UserId = userId,
+                OrganisationId = organisationId
             };
-            string analyticsJson = System.Text.Json.JsonSerializer.Serialize(analyticsObj);
-            _logger.LogInformation("{analyticsJson}", analyticsJson);
+            _logger.LogInformation("{@Analytics}", logsObj);
 
         _logger.LogInformation("{ \"action\": \"viewAccommodation\", \"accommodationId\": \"13\", \"userId\": \"11\", \"organisationId\": \"14\" }");
          _logger.LogInformation("{{ \"action\": \"{action}\", \"accommodationId\": \"{accommodationId}\", \"userId\": \"{userId}\", \"organisationId\": \"{organisationId}\" }}", "viewAccommodation", "13", "11", "14");
